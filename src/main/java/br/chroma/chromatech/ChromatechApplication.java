df7b64d4d26e807cdb4592250292f7ea3dea207a
MATHEUS FELIPE ALVES RIBEIRO <matheus_fa_ribeiro@estudante.sesisenai.org.br>
2026-09-24 19:29:57 -0300

    Initial Commit

---
+++ src/main/java/br/chroma/chromatech/ChromatechApplication.java
@@ -0,0 +1,13 @@
+package br.chroma.chromatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChromatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/chroma/chromatech/ChromatechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.chroma.chromatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChromatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
